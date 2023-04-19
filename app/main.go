3192dff24b7224ef39e3f225be006eee107ad00c
Moby von Briesen <mobyvb@gmail.com>
2023-04-19 00:26:03 -0400

    Initial commit
    GPT-4: In the index.html file, I have added a <style> tag in the <head> section to define basic CSS styles for the body and header. The header "moby's cool app" is now wrapped in an <h1> tag.
    In the main.go file, I have created a simple Go server to serve the index.html file. The server listens on port 8080 and serves the content of index.html for the root path ("/"). The file is read using ioutil.ReadFile and then written to the ResponseWriter.

---
+++ app/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		content, err := ioutil.ReadFile("index.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write(content)
+	})
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
